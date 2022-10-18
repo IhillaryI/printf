75be7786dc2a078e431543489c3a63089be9eca0
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-18 03:43:30 +0000

    modified file

--- _printf.c
+++ _printf.c
@@ -1,6 +1,10 @@
 #include <unistd.h>
 #include <stdarg.h>
 
+void
+fmt_spec(va_list ap, const char *fmt, unsigned int *i, unsigned int *count);
+
+
 /**
 * _printf - printf formatted string
 * @format: formatted string
@@ -8,8 +12,7 @@
 */
 int _printf(const char *format, ...)
 {
-	unsigned int count, i, j, k;
-	char c, *s;
+	unsigned int count, i;
 	va_list ap;
 
 	count = 0;
@@ -19,37 +22,7 @@ int _printf(const char *format, ...)
 	{
 		if (*(format + i) == '%')
 		{
-			j = i + 1;
-			while (*(format + j) == ' ')
-				++j;
-			if (*(format + j) == 's')
-			{
-				s = va_arg(ap, char *);
-				k = 0;
-				while (*(s + k))
-					++k;
-				count += k;
-				write(1, s, k);
-				i = j;
-			}
-			else if (*(format + j) == 'c')
-			{
-				c = va_arg(ap, int);
-				write(1, &c, 1);
-				++count;
-				i = j;
-			}
-			else if (*(format + j) == '%')
-			{
-				write(1, (format + j), 1);
-				i = j;
-				++count;
-			}
-			else
-			{
-				write(1, (format + i), 1);
-				++count;
-			}
+			fmt_spec(ap, format, &i, &count);
 			++i;
 		}
 		else
@@ -64,4 +37,48 @@ int _printf(const char *format, ...)
 	return (count);
 }
 
+/**
+* fmt_spec - print specifier
+* @fmt: format string
+* @ap: type of va_list
+* @i: unsigned int
+* @count: unsigned int
+*/
+void
+fmt_spec(va_list ap, const char *fmt, unsigned int *i, unsigned int *count)
+{
+	char *s, c;
+	int j, k;
 
+	j = *i + 1;
+	while (*(fmt + j) == ' ')
+		++j;
+	if (*(fmt + j) == 's')
+	{
+		s = va_arg(ap, char *);
+		k = 0;
+		while (*(s + k))
+			++k;
+		*count += k;
+		write(1, s, k);
+		*i = j;
+	}
+	else if (*(fmt + j) == 'c')
+	{
+		c = va_arg(ap, int);
+		write(1, &c, 1);
+		++*count;
+		*i = j;
+	}
+	else if (*(fmt + j) == '%')
+	{
+		write(1, (fmt + j), 1);
+		*i = j;
+		++*count;
+	}
+	else
+	{
+		write(1, (fmt + *i), 1);
+		++*count;
+	}
+}
