8eae3fad2886fa19f7dc3d52b3ba41ba67cf7d3e
IhillaryI <ifeanyichukwuhillary2019@gmail.com>
2022-10-18 12:07:43 +0000

    modified file

--- _printf.c
+++ _printf.c
@@ -1,9 +1,12 @@
 #include <unistd.h>
+#include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 void
 fmt_spec(va_list ap, const char *fmt, unsigned int *i, unsigned int *count);
-
+char *itoa(int value);
+int digit(int value);
 
 /**
 * _printf - printf formatted string
@@ -72,6 +75,11 @@ fmt_spec(va_list ap, const char *fmt, unsigned int *i, unsigned int *count)
 		++*count;
 		*i = j;
 	}
+	else if (*(fmt + j) == 'd' || *(fmt + j) == 'i')
+	{
+		*count += digit(va_arg(ap, int));
+		*i = j;
+	}
 	else if (*(fmt + j) == '%')
 	{
 		write(1, (fmt + j), 1);
@@ -83,4 +91,58 @@ fmt_spec(va_list ap, const char *fmt, unsigned int *i, unsigned int *count)
 		write(1, (fmt + *i), 1);
 		++*count;
 	}
+}
+
+/**
+* itoa - convert an int to a string
+* @value: integer value
+* Return: a string of numbers
+*/
+char *itoa(int value)
+{
+	unsigned int count;
+	int copy;
+	char *str;
+
+	count = 0;
+	copy = value;
+	while (copy)
+	{
+		copy /= 10;
+		++count;
+	}
+	str = malloc(sizeof(char) * (count + 1));
+	if (str != NULL)
+	{
+		str[count] = '\0';
+		while (value)
+		{
+			str[--count] = (value % 10) + '0';
+			value /= 10;
+		}
+	}
+	return (str);
+}
+
+/**
+* digit - handle digit spec
+* @value: integer value
+* Return: the string count, 0 if s is NULL
+*/
+int digit(int value)
+{
+	char *s;
+	int k;
+
+	k = 0;
+	s = itoa(value);
+	if (s != NULL)
+	{
+		while (*(s + k))
+			++k;
+		write(1, s, k);
+		free(s);
+		return (k);
+	}
+	return (0);
 }
